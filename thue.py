ba9884a6ead13a8a9d7495624a39b3ff4ccba10f
Coolperson <swann.dp@gmail.com>
2014-06-19 11:20:46 -0400

    initial commit

---
+++ thue.py
@@ -0,0 +1,60 @@
+import random
+import sys
+
+
+CAN_BE = "::="
+INPUT_STREAM = ":::"
+OUTPUT_STREAM = "~"
+
+
+class ThueParser:
+	def __init__(self):
+		self.rules = []
+		self.current = []
+		self.statement = None
+
+	def build(self, stream):
+		lines = stream.split("\n")
+		rules_complete = False
+		for line in lines:
+			if line.find(CAN_BE) > 0:
+				self.rules.append(line.split(CAN_BE))
+			elif line.find(CAN_BE) == 0:
+				rules_complete = True
+				continue
+			if rules_complete and len(line) > 0:
+				self.statement = line
+				break
+		self.current = list(self.rules)
+
+	def step(self):
+		index = random.randint(0, len(self.current) - 1)
+		lhs = self.current[index][0]
+		rhs = self.current[index][1]
+		if lhs in self.statement:
+			if rhs[0] == OUTPUT_STREAM:
+				print(rhs[1:])
+				self.statement = self.statement.replace(lhs, "", 1)
+			elif rhs[0] == INPUT_STREAM:
+				query = input("%s=" % lhs)
+				self.statement = self.statement.replace(lhs, query, 1)
+			else:
+				self.statement = self.statement.replace(lhs, rhs, 1)
+			self.current = list(self.rules)
+		else:
+			self.current.pop(index)
+	def execute(self):
+		while len(self.current) > 0:
+			self.step()
+
+
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		t = ThueParser()
+		with open(sys.argv[1]) as f:
+			t.build(f.read())
+		t.execute()
+		print(t.statement)
+	else:
+		print("thue.py <filename>")
+		sys.exit(1)
